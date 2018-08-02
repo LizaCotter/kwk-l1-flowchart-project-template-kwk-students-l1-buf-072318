dd839c307fb30474d37f6bb8cd214f81fc186bdf
LizaCotter <ecotter20@allendalecolumbia.org>
2018-08-02 21:08:56 +0000

    working version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,18 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
+  
+  get '/imagelink' do
+    erb :deepfry
+  end
 
+  # Add your post route and action be
+  post '/imagefinal' do
+    print params.to_s
+    @link = params[:link]
+    @height = params[:height] 
+    @width = params[:width] 
+    @color = params[:color]
+    erb :imagefinal
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,33 @@
-class Question
+# def colorlogic(@color)
+# if @color.include? "#"
+#   @color = @color
+  
+# elsif @color.downcase == "red"
+#   @color = r:0 g:204 b:204
+  
+# elsif @color.downcase == "red"
+#   @color = r:153 g:204 b:255
+  
+# elsif @color.downcase == "orange"
+#   @color = r:0 g:102 b:204
+  
+# elsif @color.downcase == "yellow"
+#   @color = r:102 g:102 b:255
+  
+# elsif @color.downcase == "green"
+#   @color = r:153 g:255 b:255
+  
+# elsif @color.downcase == "lime"
+#   @color = r:204 g:153 b:255
+  
+# elsif @color.downcase == "cyan"
+#   @color = r:255 g:153 b:153
+  
+# elsif @color.downcase == "blue"
+#   @color = r:255 g:204 b:153
+  
+# elsif @color.downcase == "purple"
+#   @color = r:153 g:255 b:153
+# end
+# end
 
-end
